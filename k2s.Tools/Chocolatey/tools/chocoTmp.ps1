3bb02c6e45b85202a0c1444190543098df5bd113
Davide <davide.maggi@live.com>
2022-11-01 11:57:04 +0100

    revmoved x86

--- k2s.Tools/Chocolatey/tools/chocoTmp.ps1
+++ k2s.Tools/Chocolatey/tools/chocoTmp.ps1
@@ -3,18 +3,18 @@ $ErrorActionPreference = 'Stop';
 $packageName= 'k2s'
 $version = '@@VERSION@@'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "https://github.com/davidemaggi/k2s/releases/download/$version/k2s-$version-win-x86-signed.zip"
+#$url        = "https://github.com/davidemaggi/k2s/releases/download/$version/k2s-$version-win-x86-signed.zip"
 $url64        = "https://github.com/davidemaggi/k2s/releases/download/$version/k2s-$version-win-x64-signed.zip"
-$hash = '@@HASH_X86@@'
+#$hash = '@@HASH_X86@@'
 $hash64 = '@@HASH_X64@@'
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url64bit      = $url64
-  url           = $url
-  checksum      = $hash
+#  url           = $url
+#  checksum      = $hash
   checksum64    = $hash64
-  checksumType  = 'MD5'
+#  checksumType  = 'MD5'
   checksumType64  = 'MD5'
 }
 
