9d8c1d2f80c217b7b728aa166fb5d2459bc681c8
Davide <davide.maggi@live.com>
2022-11-01 11:58:29 +0100

    asdadsd

--- k2s.Tools/Chocolatey/publish.ps1
+++ k2s.Tools/Chocolatey/publish.ps1
@@ -16,12 +16,9 @@ $tagStrip=$tag.substring(1)
 $x64File="k2s-$($tag)-win-x64-signed.zip"
 $x64Url= "https://github.com/davidemaggi/k2s/releases/download/$($tag)/$($x64File)"
 
-$x86File="k2s-$($tag)-win-x86-signed.zip"
-$x86Url= "https://github.com/davidemaggi/k2s/releases/download/$($tag)/$($x86File)"
 
 # Downloaad the binaries
 Invoke-WebRequest -URI $x64Url -OutFile $x64File
-Invoke-WebRequest -URI $x86Url -OutFile $x86File
 
 #Create Choco Files
 $nuspec=Get-Content -Path .\tmp.nuspec
@@ -33,11 +30,10 @@ Out-File -FilePath .\k2s.nuspec -InputObject $nuspec
 
 
 $md5x64= Get-FileHash $x64File -Algorithm MD5
-$md5x86= Get-FileHash $x86File -Algorithm MD5
 
 $chocoScript=$chocoScript.Replace("@@VERSION@@",$tag)+""
 $chocoScript=$chocoScript.Replace("@@HASH_X64@@",$md5x64.Hash)+""
-$chocoScript=$chocoScript.Replace("@@HASH_X86@@",$md5x86.Hash)+""
+
 
 Out-File -FilePath .\tools\chocolateyinstall.ps1 -InputObject $chocoScript
 
